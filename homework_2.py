16ae91f4045e31509c4df98b72bb944b90e90287
Artem <ArtemKrushev1@gmail.com>
2025-08-03 11:52:32 +0300

    fix: Correct quantity validation in homework_2.py

--- homework_2.py
+++ homework_2.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min_num, max_num, quantity):
 
     # Перевірка вхідних параметрів
-    if not (1 <= min_num <= max_num <= 1000) or not (min_num < quantity < max_num):
+    if not (1 <= min_num <= max_num <= 1000) or not (1 <= quantity <= max_num - min_num + 1):
         return []
     
     numbers = set()
